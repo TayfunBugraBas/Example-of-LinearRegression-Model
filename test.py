a27050314cc4e98640e5bc132c73d2c5604563c6
TayfunBugraBas <73062585+TayfunBugraBas@users.noreply.github.com>
2021-06-29 14:33:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -13,19 +15,38 @@ predict = "G3"
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.37)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test, y_test)
+    linear.fit(x_train, y_train)
+    accuracy = linear.score(x_test, y_test)
 
-print(accuracy)
+    print(accuracy)
 
-print("Co: ", linear.coef_ )
-print("Intercept : ", linear.intercept_ )
+    if accuracy > best:
+        best = accuracy
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print("Co: ", linear.coef_)
+print("Intercept : ", linear.intercept_)
 
 predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
-     print(predictions[x], x_test[x], y_test[x])
+    print(predictions[x], x_test[x], y_test[x])
+
+p = 'G1'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
+
